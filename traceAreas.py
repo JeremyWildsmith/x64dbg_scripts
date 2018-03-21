92d89aa8cd1a1f279aef989cd18a9d59b9f53b04
Jeremy Wildsmith <JeremyWildsmith@yahoo.ca>
2018-03-21 00:29:05 -0700

    Optimized trace areas, added sample flow.graphml.

--- traceAreas.py
+++ traceAreas.py
@@ -22,77 +22,15 @@ class Label(object):
     def __init__(self, labelText):
         self.labelText = labelText
 
-UNCHANGED_IGNORE_THRESHOLD = 30
+UNCHANGED_IGNORE_THRESHOLD = 3
 
 queryStop = False
 
 traceAreaArray = [
-    TA(0x866f2c),
-    TA(0x92d42c),
-    TA(0x91c5d4),
-    TA(0x8e2946),
-    TA(0x98b285),
-    TA(0x8f83ac),
-    TA(0x882199),
-    TA(0x8c1b18),
-    TA(0x90975f),
-    TA(0x99d866),
-    TA(0xa0a4a7),
-    TA(0x9707e4),
-    TA(0x9a0b4e),
-    TA(0x87c1aa),
-    TA(0x99a292),
-    TA(0x98ef6a),
-    TA(0x8ee7ea),
-    TA(0x9e9c99),
-    TA(0xa3e60e),
-    TA(0x8d0adb),
-    TA(0x929159),
-    TA(0x89b01d),
-    TA(0x928d09),
-    TA(0xa0bf0d),
-    TA(0x98a28a),
-    TA(0x972fe8),
-    TA(0xa3210e),
-    TA(0x9acbdd),
-    TA(0x8ba1c5),
-    TA(0xa3f5de),
-    TA(0x8a670c),
-    TA(0x911d94),
-    TA(0x932587),
-    TA(0x971970),
-    TA(0x9c5bd6),
-    TA(0x8e521c),
-    TA(0x98dbe0),
-    TA(0x8868dc),
-    TA(0x8c9bed),
-    TA(0x8673a2),
-    TA(0xa3df9c),
-    TA(0x953e3f),
-    TA(0x926a9d),
-    TA(0x97c57b),
-    TA(0x9ec97c),
-    TA(0x9c2abd),
-    TA(0xa210b3),
-    TA(0x998822),
-    TA(0x992afb),
-    TA(0x88c0a0),
-    TA(0x8ac217),
-    TA(0x8c30b4),
-    TA(0x9daf55),
-    TA(0x9db3f1),
-    TA(0x8d0f58),
-    TA(0x88c21a),
-    TA(0x8ac3a9),
-    TA(0x8c3219),
-    TA(0x8e3e6a),
-    TA(0x99f09b),
-    TA(0x8e560a),
-    TA(0x926c07),
-    TA(0x971e22),
-    TA(0x9328c7),
-    TA(0xa25c5d),
-    TA(0xa2603c),
+        TA(0x9bc113),
+        TA(0x9707e4),
+        TA(0x91ae8e),
+        TA(0x999130),
     ]
 
 installAddr = 0x00A250CD
@@ -102,18 +40,28 @@ def read_buffer(addr, size):
     result, read_size = x64dbgpy.pluginsdk.x64dbg.Memory_Read(addr, read_bytes, size)
     return bytes(read_bytes[:read_size])
 
+def findBranch(ta, size):
+    memory = read_buffer(ta.address, size)
+    decomposedInstructions = distorm3.Decompose(ta.address, memory)
+        
+    for inst in decomposedInstructions:
+        if (inst.flowControl == "FC_UNC_BRANCH") and (inst.operands[0].type == "Register"):
+            ta.jumpAddress = inst.address
+            return True
+        elif (inst.flowControl == "FC_RET"):
+            ta.jumpAddress = inst.address
+            return True
+
+    return False
+
 def resolveTraceAreas():
     global traceAreaArray
 
     for ta in traceAreaArray:
         longestAddress = 0
-        memory = read_buffer(ta.address, 2000)
-        decomposedInstructions = distorm3.Decompose(ta.address, memory)
-        
-        for inst in decomposedInstructions:
-            if (inst.flowControl == "FC_UNC_BRANCH") or (inst.flowControl == "FC_RET"):
-                ta.jumpAddress = inst.address
-                break
+        if not findBranch(ta, 2000):
+            if not findBranch(ta, 50000):
+                raise Exception("Unable to resolve TA: " + hex(ta.address))
 
         ta.size = ta.jumpAddress - ta.address
     
